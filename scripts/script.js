69e35ecd82f0950909fdaf0edcdb275494ff161c
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-03 15:40:06 -0500

    Added functionality to calculate tip

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+// Custome function
+
+function calculateTip(){
+
+  //store data of inputs
+  var billAmount = document.getElementById("billAmount").value;
+  var serviceQuality = document.getElementById("serviceQuality").value;
+  var numPeople = document.getElementById("totalPeople").value;
+
+  // Quick validation
+  if(billAmount === "" || serviceQuality === 0){
+    window.alert("Please enter some values");
+    return;
+  }
+
+  // Check to see if input is empty or less than or equal to 1
+  if(numPeople === "" || numPeople <= 1){
+    numPeople = 1;
+
+    document.getElementById("each").style.display = "none";
+  }else{
+    document.getElementById("each").style.display = "block";
+  }
+
+  // Do some math
+  var total = (billAmount*serviceQuality) / numPeople;
+  total = Math.round(total*100) / 100;
+  total = total.toFixed(2);
+
+  //Display the tip
+
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+
+};
+
+
+
+
+// Hide Tip amount on load
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+
+//Clicking the button calls our custom function
+
+document.getElementById("calculate").onclick = function(){
+  calculateTip();
+};
